273a0444729f3074e75da55dcc0b3c4f9d5fd6ab
ROGi0228 <84007090+ROGi0228@users.noreply.github.com>
2021-07-17 13:49:55 +0600

    Add files via upload

---
+++ components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="header">
+        <img src="static/assets/images/ЯЧ.png" alt="" class="logo">
+        
+    </div>
+    
+</template>
+
+<style scoped>
+    .logo{
+        width: 300px;
+    }
+
+    
+
+
+</style>
